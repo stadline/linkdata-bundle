aa9df66b2aef9debc7f8496bba7452a2b3c3a11a
Dimzeraw <dimitri.warez@stadline.com>
2016-05-10 14:33:52 +0200

    Optimize Level Utils

--- Utils/Level.php
+++ Utils/Level.php
@@ -4,22 +4,31 @@ namespace Geonaute\LinkdataBundle\Utils;
 
 class Level
 {
+
+    /**
+     * Get level from $score value
+     *
+     * @param integer $score
+     * @return integer
+     */
     public static function getLevelFromPointEarned($score)
     {
-        if ($score > 150000) {
-            return 7;
-        } elseif ($score > 100000) {
-            return 6;
-        } elseif ($score > 50000) {
-            return 5;
-        } elseif ($score > 25000) {
-            return 4;
-        } elseif ($score > 10000) {
-            return 3;
-        } elseif ($score > 3000) {
-            return 2;
-        } else {
-            return 1;
+        switch ($score) {
+            case ($score > 150000):
+                return 7;
+            case ($score > 100000):
+                return 6;
+            case ($score > 50000):
+                return 5;
+            case ($score > 25000):
+                return 4;
+            case ($score > 10000):
+                return 3;
+            case ($score > 3000):
+                return 2;
+            default:
+                return 1;
         }
     }
+
 }
