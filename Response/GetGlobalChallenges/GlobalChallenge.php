2b2e7ae8a26e0361848fa78f41305ccde934570c
Dimzeraw <dimitri.warez@stadline.com>
2016-05-12 09:15:46 +0200

    Add GetGlobalChallenges operation

---
+++ Response/GetGlobalChallenges/GlobalChallenge.php
@@ -0,0 +1,315 @@
+<?php
+
+namespace Geonaute\LinkdataBundle\Response\GetGlobalChallenges;
+
+use Geonaute\LinkdataBundle\Utils\Datatype;
+// use Geonaute\Widget\GlobalChallengeBundle\Mapper\GlobalChallengeWidgetObjectInterface; // @todo Reimplement this
+use JMS\Serializer\Annotation as Serializer;
+
+class GlobalChallenge implements GlobalChallengeWidgetObjectInterface
+{
+
+    /**
+     * @Serializer\SerializedName("TOKEN")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $token;
+
+    /**
+     * @Serializer\SerializedName("NAME")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @Serializer\SerializedName("BEFORE_MESSAGE")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $beforeMessage;
+
+    /**
+     * @Serializer\SerializedName("CURRENT_MESSAGE")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $currentMessage;
+
+    /**
+     * @Serializer\SerializedName("AFTER_MESSAGE")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $afterMessage;
+
+    /**
+     * @Serializer\SerializedName("PUBLISH_DATE")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $publishDate;
+
+    /**
+     * @Serializer\SerializedName("STARTED_AT")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $startedAt;
+
+    /**
+     * @Serializer\SerializedName("ENDED_AT")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $endedAt;
+
+    /**
+     * @Serializer\SerializedName("RESULT")
+     * @Serializer\Type("integer")
+     *
+     * @var integer
+     */
+    private $result;
+
+    /**
+     * @Serializer\SerializedName("TARGET")
+     * @Serializer\Type("integer")
+     *
+     * @var integer
+     */
+    private $target;
+
+    /**
+     * @Serializer\SerializedName("TARGET_UNIT_ID")
+     * @Serializer\Type("integer")
+     *
+     * @var integer
+     */
+    private $targetUnitId;
+
+    /**
+     * @Serializer\SerializedName("IMAGE_URL")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $imageUrl;
+
+    /**
+     * @Serializer\SerializedName("CURRENT")
+     * @Serializer\Type("boolean")
+     *
+     * @var boolean
+     */
+    private $current;
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->getName();
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBeforeMessage()
+    {
+        return $this->beforeMessage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrentMessage()
+    {
+        return $this->currentMessage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAfterMessage()
+    {
+        return $this->afterMessage;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPublishDate()
+    {
+        return $this->publishDate;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStartedAt()
+    {
+        return $this->startedAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getStartedAtAsDateTimeObject()
+    {
+        return \DateTime::createFromFormat('Y-m-d H:i:s', $this->startedAt);
+    }
+
+    /**
+     * @return string
+     */
+    public function getEndedAt()
+    {
+        return $this->endedAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getEndedAtAsDateTimeObject()
+    {
+        return \DateTime::createFromFormat('Y-m-d H:i:s', $this->endedAt);
+    }
+
+    /**
+     * @return integer
+     */
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getTarget()
+    {
+        return $this->target;
+    }
+
+    /**
+     * @return float between 0 and 100
+     */
+    public function getProgress()
+    {
+        return 100 * $this->result / $this->target;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getTargetUnitId()
+    {
+        return $this->targetUnitId;
+    }
+
+    /**
+     * @return \Geonaute\Module\LinkdataBundle\Service\GetReferenceDatatypes\Datatype
+     */
+    public function getTargetUnit()
+    {
+        $client = $this->response->getClient();
+        $response = $client->getReferenceDatatypes();
+
+        return $response->getDatatypes()->offsetGet($this->targetUnitId);
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageUrl()
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * Returns challenge state (BEFORE / CURRENT / AFTER)
+     *
+     * @return integer
+     */
+    public function getState()
+    {
+        $now = new \DateTime();
+        $startedAt = $this->getStartedAtAsDateTimeObject();
+        $endedAt = $this->getEndedAtAsDateTimeObject();
+
+        if ($startedAt > $now) {
+            return self::STATE_BEFORE;
+        } elseif ($this->endedAt && $now >= $endedAt) {
+            return self::STATE_AFTER;
+        } else {
+            return self::STATE_CURRENT;
+        }
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isOver()
+    {
+        return ($this->getState() == self::STATE_AFTER);
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isInProgress()
+    {
+        return ($this->getState() == self::STATE_CURRENT);
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isNotStarted()
+    {
+        return ($this->getState() == self::STATE_BEFORE);
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isDistance()
+    {
+        return ($this->getTargetUnitId() == Datatype::DISTANCE);
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getCurrent()
+    {
+        return $this->current;
+    }
+
+}
---
+++ Response/GetGlobalChallenges/Response.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Geonaute\Module\LinkdataBundle\Service\GetGlobalChallenges;
+
+use Geonaute\Module\LinkdataBundle\Service\ClientAwareXmlResponse;
+
+class Response extends ClientAwareXmlResponse
+{
+
+    /**
+     * @Serializer\SerializedName("GLOBALCHALLENGES")
+     * @Serializer\XmlList(entry="GLOBALCHALLENGE")
+     * @Serializer\Type("array<Geonaute\LinkdataBundle\Response\GetGlobalChallenges\Challenge>")
+     *
+     * @var array
+     */
+    private $globalChallenges;
+
+    /**
+     * @return array
+     */
+    public function getGlobalChallenges()
+    {
+        return $this->globalChallenges;
+    }
+
+}
