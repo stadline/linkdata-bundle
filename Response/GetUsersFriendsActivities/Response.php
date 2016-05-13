e536e401ddd7267f24d285fc7cb7c7a92fa81edf
Dimzeraw <dimitri.warez@stadline.com>
2016-05-13 16:04:33 +0200

    Add GetUsersFriendsActivities*
    @todo Add Response called from them

---
+++ Response/GetUsersFriendsActivities/Response.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Geonaute\LinkdataBundle\Response\GetUsersFriendsActivities;
+
+use Geonaute\LinkdataBundle\Response\GetUsersActivities\Response as UsersActivitiesResponse;
+
+class Response extends UsersActivitiesResponse
+{
+
+}
---
+++ Response/GetUsersFriendsActivitiesTags/Response.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Geonaute\LinkdataBundle\Response\GetUsersFriendsActivitiesTags;
+
+use Geonaute\LinkdataBundle\Response\GetUsersActivitiesTags\Response as UsersActivitiesTagsResponse;
+
+class Response extends UsersActivitiesTagsResponse
+{
+
+}
