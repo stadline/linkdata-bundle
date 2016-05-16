925c041db4398e8ffc7279c3903fcb09b4c488a6
Dimzeraw <dimitri.warez@stadline.com>
2016-05-16 11:51:46 +0200

    Add PutActivityImport (probably not working)
    Use param xml to inject directly xml, probably not working with new system

---
+++ Response/PutActivityImport/Response.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Geonaute\LinkdataBundle\Response\PutActivityImport;
+
+use Geonaute\LinkdataBundle\Response\Response as BaseResponse;
+
+class Response extends BaseResponse
+{
+    
+}
