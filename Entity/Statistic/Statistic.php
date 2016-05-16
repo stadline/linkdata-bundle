5c108c68320440d0ea2614d6872b63a6b62b51b7
Dimzeraw <dimitri.warez@stadline.com>
2016-05-16 11:39:25 +0200

    Add GetStatisticsTags

--- Entity/Statistic/Statistic.php
+++ Entity/Statistic/Statistic.php
@@ -4,19 +4,62 @@ namespace Geonaute\LinkdataBundle\Entity\Statistic;
 
 use JMS\Serializer\Annotation as Serializer;
 
-/**
- * Statistic entity.
- */
 class Statistic
 {
-    /**
-     * @Serializer\Type("integer")
-     * @Serializer\SerializedName("ID")
-     */
-    protected $id;
 
-    public function getId()
+    /**
+     * @Serializer\SerializedName("TAGS")
+     * @Serializer\XmlList(entry = "TAG")
+     * @Serializer\Type("ArrayCollection<Geonaute\LinkdataBundle\Entity\Statistic\Tag>")
+     *
+     * @var array<Tag>
+     */
+    private $tags;
+
+    /**
+     * @Serializer\SerializedName("VERSION")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $version;
+
+    /**
+     * @Serializer\SerializedName("CALCULATED_AT")
+     * @Serializer\Type("string")
+     *
+     * @var string
+     */
+    private $calculatedAt;
+
+    /**
+     * Return tags Object
+     *
+     * @return array<Tag>
+     */
+    public function getTags()
     {
-        return $this->id;
+        return $this->tags;
     }
+
+    /**
+     * Return the version of the calculation
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * Return the date of stats calculation
+     *
+     * @return string
+     */
+    public function getCalculatedAt()
+    {
+        return $this->calculatedAt;
+    }
+
 }
