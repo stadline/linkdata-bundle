0a258c9f5914cda044fe70a59de38bca6966093d
Thomas Glachant <thomas@glachant.fr>
2016-06-06 11:20:53 +0200

    Fix type error (mygeonaute)

--- Listener/SerializerEventSubscriber.php
+++ Listener/SerializerEventSubscriber.php
@@ -5,6 +5,7 @@ namespace Geonaute\LinkdataBundle\Listener;
 use Geonaute\LinkdataBundle\Hydrator\HydratableInterface;
 use Geonaute\LinkdataBundle\Hydrator\LinkdataHydrator;
 use Geonaute\LinkdataBundle\Plugin\CachedClient;
+use Guzzle\Service\Client;
 use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
 use JMS\Serializer\EventDispatcher\ObjectEvent;
 use JMS\Serializer\EventDispatcher\PreDeserializeEvent;
@@ -18,9 +19,9 @@ class SerializerEventSubscriber implements EventSubscriberInterface
     private $hydrator;
 
     /**
-     * @param CachedClient $client
+     * @param Client $client
      */
-    public function setClient(CachedClient $client)
+    public function setClient(Client $client)
     {
         $this->client = $client;
     }
@@ -43,10 +44,6 @@ class SerializerEventSubscriber implements EventSubscriberInterface
                 'event' => 'serializer.post_deserialize',
                 'method' => 'onPostDeserialize'
             ),
-            array(
-                'event' => 'serializer.pre_deserialize',
-                'method' => 'onPreDeserialize'
-            ),
         );
     }
 
@@ -57,32 +54,6 @@ class SerializerEventSubscriber implements EventSubscriberInterface
     {
         if ($event->getObject() instanceof HydratableInterface) {
             $event->getObject()->setHydrator($this->hydrator);
-        }
-    }
-
-    /**
-     * @param PreDeserializeEvent $event
-     */
-    public function onPreDeserialize(PreDeserializeEvent $event)
-    {
-        $mapping = array(
-            "Geonaute\\LinkdataBundle\\Entity\\Activity\\SimilarActivity" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Activity\\HydratableSimilarActivity",
-            "Geonaute\\LinkdataBundle\\Entity\\Activity\\SummaryActivity" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Activity\\HydratableSummaryActivity",
-            "Geonaute\\LinkdataBundle\\Entity\\Activity\\FullActivity" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Activity\\HydratableFullActivity",
-            "Geonaute\\LinkdataBundle\\Entity\\Common\\Value" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Common\\HydratableValue",
-            "Geonaute\\LinkdataBundle\\Entity\\Common\\FakeValue" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Common\\HydratableFakeValue",
-            "Geonaute\\LinkdataBundle\\Entity\\Users\\ActivitiesSport" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Users\\HydratableActivitiesSport",
-            "Geonaute\\LinkdataBundle\\Entity\\Users\\Activity" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Users\\HydratableActivity",
-            "Geonaute\\LinkdataBundle\\Entity\\Common\\GlobalChallenge" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Common\\HydratableGlobalChallenge",
-            "Geonaute\\LinkdataBundle\\Entity\\Shares\\GlobalChallenge" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Shares\\HydratableGlobalChallenge",
-            "Geonaute\\LinkdataBundle\\Entity\\Users\\GlobalChallenge" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\Users\\HydratableGlobalChallenge",
-            "Geonaute\\LinkdataBundle\\Entity\\GlobalChallenge\\GlobalChallenge" => "Geonaute\\LinkdataBundle\\Decorator\\Entity\\GlobalChallenge\\HydratableGlobalChallenge",
-        );
-
-        $class = $event->getType()["name"];
-
-        if (isset($mapping[$class])) {
-            $event->setType($mapping[$class], $event->getType()["params"]);
         }
     }
 }
