4606ee25cdb190b127029de50b65aea9cafcdbb3
Thomas Glachant <thomas@glachant.fr>
2016-06-08 20:55:55 +0200

    Remove using formatter in formatter

--- Formatter/FormatterManager.php
+++ Formatter/FormatterManager.php
@@ -34,70 +34,70 @@ class FormatterManager
             case Datatype::ELEVATION_MIN:
             case Datatype::ASCENT:
             case Datatype::DESCENT:
-                return $this->formatter->getElevationFormatter();
+                return $this->getElevationFormatter();
 
             case Datatype::LAP:
-                return $this->formatter->getDefaultFormatter();
+                return $this->getDefaultFormatter();
 
             case Datatype::HR_PERCENTAGE_MAX:
             case Datatype::HR_PERCENTAGE_MIN:
-                return $this->formatter->getHeartratePercentageFormatter();
+                return $this->getHeartratePercentageFormatter();
 
             case Datatype::RPM_CURRENT:
             case Datatype::RPM_AVG:
-                return $this->formatter->getRPMFormatter();
+                return $this->getRPMFormatter();
 
             case Datatype::SPEED_AVG:
             case Datatype::SPEED_CURRENT:
             case Datatype::SPEED_MIN:
             case Datatype::SPEED_MAX:
-                return $this->formatter->getSpeedFormatter();
+                return $this->getSpeedFormatter();
 
             case Datatype::DISTANCE:
-                return $this->formatter->getDistanceFormatter();
+                return $this->getDistanceFormatter();
 
             case Datatype::DURATION:
             case Datatype::ACTIVE_TIME:
             case Datatype::ELAPSED_TIME:
-                return $this->formatter->getDurationFormatter();
+                return $this->getDurationFormatter();
 
             case Datatype::SPEED_RATE_CURRENT:
             case Datatype::SPEED_RATE_AVG:
-                return $this->formatter->getSpeedRateFormatter();
+                return $this->getSpeedRateFormatter();
 
             case Datatype::HR_AVG:
             case Datatype::HR_CURRENT:
             case Datatype::HR_MIN:
             case Datatype::HR_MAX:
-                return $this->formatter->getHealthRateFormatter();
+                return $this->getHealthRateFormatter();
 
             case Datatype::CALORIES_BURNT:
-                return $this->formatter->getCalorieFormatter();
+                return $this->getCalorieFormatter();
 
             case Datatype::POINTS_EARNED:
-                return $this->formatter->getPointFormatter();
+                return $this->getPointFormatter();
 
             case Datatype::ACTIVITY_NUMBER:
-                return $this->formatter->getActivityNumberFormatter();
+                return $this->getActivityNumberFormatter();
 
             case Datatype::STEP_NUMBER:
-                return $this->formatter->getStepNumberFormatter();
+                return $this->getStepNumberFormatter();
 
             case Datatype::CURRENT_HOME_TRAINER_POWER:
             case Datatype::AVERAGE_HOME_TRAINER_POWER:
-                return $this->formatter->getPowerFormatter();
+                return $this->getPowerFormatter();
 
             case Datatype::BIKE_TRAINER_RESISTANCE:
-                return $this->formatter->getResistanceFormatter();
+                return $this->getResistanceFormatter();
 
             case Datatype::BODY_BONE_PERCENTAGE:
             case Datatype::BODY_FAT_PERCENTAGE:
             case Datatype::BODY_MUSCLE_PERCENTAGE:
             case Datatype::BODY_WATER_PERCENTAGE:
-                return $this->formatter->getPercentageFormatter();
+                return $this->getPercentageFormatter();
 
             default:
-                return $this->formatter->getDefaultFormatter();
+                return $this->getDefaultFormatter();
         }
     }
 }
