b8be524240340d4ccf93e4db0e59fe5e8bd9bca6
Dimzeraw <dimitri.warez@stadline.com>
2016-06-08 17:49:45 +0200

    Add more tests on GetUsersMeasures

--- Mock/Model/GetUsersMeasuresMock.php
+++ Mock/Model/GetUsersMeasuresMock.php
@@ -31,8 +31,12 @@ class GetUsersMeasuresMock implements LinkdataMockInterface
 			<VALUE updated_time="2011-10-20 14:36:47">70</VALUE>
 		</MEASURE>
 		<MEASURE unitid="100">
-			<VALUE updated_time="2011-11-07 17:24:05">180</VALUE>
-			<VALUE updated_time="2011-11-07 17:25:06">181</VALUE>
+                        <VALUE date="2011-11-06">180</VALUE>
+			<VALUE updated_time="2011-11-07 17:25:07">181</VALUE>
+                        <VALUE date="2011-11-08" updated_time="2011-11-08 17:24:06">182</VALUE>
+                        <VALUE date="2011-11-08" updated_time="2011-11-08 17:24:05">183</VALUE>
+                        <VALUE updated_time="2011-11-08 17:25:06">184</VALUE>
+                        <VALUE date="2011-11-09" updated_time="2011-11-08 17:24:06">185</VALUE>
 		</MEASURE>
 	</MEASURES>
 </RESPONSE>
--- Tests/Response/GetUsersMeasures/ResponseTest.php
+++ Tests/Response/GetUsersMeasures/ResponseTest.php
@@ -4,6 +4,7 @@ namespace Geonaute\LinkdataBundle\Tests\Response\GetUsersMeasures;
 
 use Geonaute\LinkdataBundle\Tests\Response\WebTestCase as ResponseTestCase;
 use Geonaute\LinkdataBundle\Mock\Model\GetUsersMeasuresMock;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class ResponseTest extends ResponseTestCase
 {
@@ -28,6 +29,43 @@ class ResponseTest extends ResponseTestCase
         $this->assertElementsAreIndexed($measuresCollection, 'getUnitId');
 
         $this->assertIsMeasureInUsersDirectory($firstMeasureOfCollection);
+
+        $this->verifyValuesOfMeasures($measuresCollection);
+    }
+
+    /**
+     * @param ArrayCollection $measures
+     */
+    private function verifyValuesOfMeasures(ArrayCollection $measures)
+    {
+        $measuresArray = $measures->toArray();
+
+        $this->assertArrayHasKey('22', $measuresArray);
+        $this->assertArrayHasKey('100', $measuresArray);
+
+        $valuesOfArrayWith22Key = $measuresArray[22]->getValues();
+
+        $this->assertEquals(1, count($valuesOfArrayWith22Key));
+        $this->assertArrayHasKey("2011-10-20", $valuesOfArrayWith22Key);
+        $this->assertEquals(70, $valuesOfArrayWith22Key['2011-10-20']);
+
+        $valuesOfArrayWith100Key = $measuresArray[100]->getValues();
+
+        // if attribute date only, check it's taken
+        $this->assertArrayHasKey("2011-11-06", $valuesOfArrayWith100Key);
+        $this->assertEquals(180, $valuesOfArrayWith100Key['2011-11-06']);
+
+        // if attribute updated_time only, check it's taken
+        $this->assertArrayHasKey("2011-11-07", $valuesOfArrayWith100Key);
+        $this->assertEquals(181, $valuesOfArrayWith100Key['2011-11-07']);
+
+        // Last value of date/updated_time taken
+        $this->assertArrayHasKey("2011-11-08", $valuesOfArrayWith100Key);
+        $this->assertEquals(184, $valuesOfArrayWith100Key['2011-11-08']);
+
+        // if attribute date and updated_time, check it's date taken
+        $this->assertArrayHasKey("2011-11-09", $valuesOfArrayWith100Key);
+        $this->assertEquals(185, $valuesOfArrayWith100Key['2011-11-09']);
     }
 
 }
