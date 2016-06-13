86a181ffc67660d3b0377f0c5488ac35b19c88e2
Dimzeraw <dimitri.warez@stadline.com>
2016-06-13 18:16:59 +0200

    Add missing getDevice on fullActivity + reorganize

--- Entity/Activity/FullActivity.php
+++ Entity/Activity/FullActivity.php
@@ -3,6 +3,7 @@
 namespace Geonaute\LinkdataBundle\Entity\Activity;
 
 use Geonaute\LinkdataBundle\Entity\Tracks\Track;
+use Geonaute\LinkdataBundle\Entity\Users\ConnectedDevice;
 use JMS\Serializer\Annotation as Serializer;
 
 class FullActivity extends SummaryActivity
@@ -16,29 +17,12 @@ class FullActivity extends SummaryActivity
     protected $summary;
 
     /**
-     * @return DataSummary
-     */
-    public function getSummary()
-    {
-        die("getMySy");
-        return $this->summary;
-    }
-
-    /**
      * @Serializer\SerializedName("DATASTREAM")
      * @Serializer\Type("Geonaute\LinkdataBundle\Entity\Activity\DataStream")
      *
      * @var DataStream
      */
     protected $dataStream;
-
-    /**
-     * @return DataStream
-     */
-    public function getDataStream()
-    {
-        return $this->dataStream;
-    }
 
     /**
      * @Serializer\SerializedName("TRACK")
@@ -49,10 +33,42 @@ class FullActivity extends SummaryActivity
     protected $track;
 
     /**
+     * @Serializer\SerializedName("CONNECTEDDEVICE")
+     * @Serializer\Type("Geonaute\LinkdataBundle\Entity\Users\ConnectedDevice")
+     *
+     * @var ConnectedDevice
+     */
+    protected $device;
+
+    /**
+     * @return DataSummary
+     */
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    /**
+     * @return DataStream
+     */
+    public function getDataStream()
+    {
+        return $this->dataStream;
+    }
+
+    /**
      * @return Track
      */
     public function getTrack()
     {
         return $this->track;
+    }
+
+    /**
+     * @return Track
+     */
+    public function getDevice()
+    {
+        return $this->device;
     }
 }
